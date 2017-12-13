9443f0f864c0fb52b83f2eaa3f625579f972a69c
danbond85@mail.ru <danbond85@gmail.com>
2017-12-13 23:37:38 +0300

    Released

---
+++ Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php'; 
+
+if($_GET['address'] ?? '') {
+
+    $address = $_GET['address'];
+    $addressParam = 
+
+    $api = new \Yandex\Geo\Api();
+
+    $api->setQuery($address);
+
+    $api
+        ->setLimit(10)
+        ->setLang(\Yandex\Geo\Api::LANG_RU)
+        ->load();
+
+    $response = $api->getResponse();
+
+}
+
+$hasCoords = ($_GET['lat'] ?? '') && ($_GET['lng'] ?? '');
+$mapLat = $_GET['lat'] ?? 55.751999;
+$mapLng = $_GET['lng'] ?? 37.617734;
+$mapAddress = $_GET['pureAddress'] ?? '';
