fa363d754840a1f7e96a2cacd30b74bccb583366
danbond85@mail.ru <danbond85@gmail.com>
2017-12-13 21:06:57 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="css/style.css">
+     <script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+</head>
+<body>
+    <div class="container">
+        <h1>Yandex-карты</h1>
+        <hr>
+        
+    </div>
+</body>
+</html>
